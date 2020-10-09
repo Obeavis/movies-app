f54e9711e2c901dd5f572a2ed624c43dd100f05a
Otavio Bastilho <ilegra.otavio@agibank.com.br>
2020-10-09 14:29:10 -0300

    🚀

---
+++ precache-manifest.9e02944c25f3723611fac52642ae6508.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3d913e8c0114e333b8c4010983b6f42",
+    "url": "/movies-app/index.html"
+  },
+  {
+    "revision": "cc1be2d2c3520d22b635",
+    "url": "/movies-app/static/css/main.eac5f307.chunk.css"
+  },
+  {
+    "revision": "c7f51078fc2df3969e51",
+    "url": "/movies-app/static/js/2.722a8023.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/movies-app/static/js/2.722a8023.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc1be2d2c3520d22b635",
+    "url": "/movies-app/static/js/main.0c44f28c.chunk.js"
+  },
+  {
+    "revision": "6f4ce7a2b201b6f9f6f8",
+    "url": "/movies-app/static/js/runtime-main.94bcfe81.js"
+  },
+  {
+    "revision": "a5e63aba11b0ac7546b75179414914c7",
+    "url": "/movies-app/static/media/brazil-flag.a5e63aba.png"
+  },
+  {
+    "revision": "7f9eda5fcb4ca505cc91c2eaf4549e26",
+    "url": "/movies-app/static/media/magnifier.7f9eda5f.svg"
+  },
+  {
+    "revision": "0e326f201ea49ae53272ecf98c0102d9",
+    "url": "/movies-app/static/media/star.0e326f20.svg"
+  },
+  {
+    "revision": "4d460eba7ba86f6bf02371c443d97cb1",
+    "url": "/movies-app/static/media/usa-flag.4d460eba.png"
+  }
+]);
